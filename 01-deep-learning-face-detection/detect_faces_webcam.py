6b80af9641909bccf5742eee026cc78427a340c1
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-05-25 10:41:07 +0500

    showing predictions bounding boxes and confidence

--- 01-deep-learning-face-detection/detect_faces_webcam.py
+++ 01-deep-learning-face-detection/detect_faces_webcam.py
@@ -44,3 +44,26 @@ while True:
 	# predictions
 	net.setInput(blob)
 	detections = net.forward()
+		# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence < args["confidence"]:
+			continue
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+ 
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(frame, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(frame, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		
