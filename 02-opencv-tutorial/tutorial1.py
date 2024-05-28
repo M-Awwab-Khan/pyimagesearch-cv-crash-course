d0ab9e5d1a8c194095d04b86bf1825a7c2a48d87
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-05-28 05:30:13 +0500

    resizing by maintaining aspect ratio

--- 02-opencv-tutorial/tutorial1.py
+++ 02-opencv-tutorial/tutorial1.py
@@ -28,3 +28,11 @@ cv2.waitKey(0)
 resized = cv2.resize(image, (200, 200))
 cv2.imshow("Fixed Resizing", resized)
 cv2.waitKey(0)
+
+# fixed resizing and distort aspect ratio so let's resize the width
+# to be 300px but compute the new height based on the aspect ratio
+r = 300.0 / w
+dim = (300, int(h * r))
+resized = cv2.resize(image, dim)
+cv2.imshow("Aspect Ratio Resize", resized)
+cv2.waitKey(0)
