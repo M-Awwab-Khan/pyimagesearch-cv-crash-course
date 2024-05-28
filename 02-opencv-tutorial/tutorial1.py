206cade87d034be44aecf34624581abdc9a2f108
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-05-28 05:30:44 +0500

    rotating using opencv

--- 02-opencv-tutorial/tutorial1.py
+++ 02-opencv-tutorial/tutorial1.py
@@ -42,3 +42,12 @@ cv2.waitKey(0)
 resized = imutils.resize(image, width=300)
 cv2.imshow("Imutils Resize", resized)
 cv2.waitKey(0)
+
+# let's rotate an image 45 degrees clockwise using OpenCV by first
+# computing the image center, then constructing the rotation matrix,
+# and then finally applying the affine warp
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, -45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("OpenCV Rotation", rotated)
+cv2.waitKey(0)
